ac6f9662ea6e312d15bfcb98b7d67e1309215796
ArendtKacper <arendtkacper@gmail.com>
2024-04-14 13:24:07 +0200

    update task

--- noteApp/Dtos/TaskListForRead.cs
+++ noteApp/Dtos/TaskListForRead.cs
@@ -2,6 +2,8 @@ namespace noteApp.Dtos;
 
 public class TaskListForRead(string title, List<TaskForRead> tasksList, DateTime createdOn, DateTime updatedOn)
 {
+    public Guid ListId { get; set; }
+    
     public string Title { get; set; } = title;
     
     public List<TaskForRead> TasksList { get; set; } = tasksList;
--- noteApp/Dtos/TaskListForReadBasic.cs
+++ noteApp/Dtos/TaskListForReadBasic.cs
@@ -2,6 +2,8 @@ namespace noteApp.Dtos;
 
 public class TaskListForReadBasic(string title, DateTime createdOn, DateTime updatedOn)
 {
+    public Guid ListId { get; set; }
+
     public string Title { get; set; } = title;
     
     public DateTime CreatedOn { get; set; } = createdOn;
